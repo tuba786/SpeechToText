917f0a0ecabcf33ef23269b77daeb6de49acb697
tuba786 <zehrarizvituba@gmail.com>
2022-10-29 15:42:35 +0530

    Initial commit

---
+++ app/src/main/java/com/digipod/speechtotext/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.digipod.speechtotext
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+        // on below line we are creating variables
+        // for text view and image view
+        lateinit var outputTV: TextView
+        lateinit var micIV: ImageView
+
+        // on below line we are creating a constant value
+        private val REQUEST_CODE_SPEECH_INPUT = 1
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            // initializing variables of list view with their ids.
+            outputTV = findViewById(R.id.idTVOutput)
+            micIV = findViewById(R.id.idIVMic)
+            micIV.setOnClickListener {
+                // on below line we are calling speech recognizer intent.
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+                // on below line we are passing language model
+                // and model free form in our intent
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+
+                // on below line we are passing our
+                // language as a default language.
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE,
+                    Locale.getDefault()
+                )
+
+                // on below line we are specifying a prompt
+                // message as speak to text on below line.
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+                try {
+                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+                } catch (e: Exception) {
+                    // on below line we are displaying error message in toast
+                    Toast
+                        .makeText(
+                            this@MainActivity, " " + e.message,
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+            }
+        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // in this method we are checking request
+        // code with our result code.
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            // on below line we are checking if result code is ok
+            if (resultCode == RESULT_OK && data != null) {
+
+                // in that case we are extracting the
+                // data from our array list
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                // on below line we are setting data
+                // to our output text view.
+                outputTV.setText(
+                    Objects.requireNonNull(res)[0]
+                )
+            }
+        }
+    }
+}
